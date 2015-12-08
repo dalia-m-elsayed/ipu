5bbf1ba8649f307420ee96b81b3c3a2b6a81a35e
dalia-m-elsayed <dalia.m.elsayed@gmail.com>
2015-12-08 15:25:49 +0400

    first commit

---
+++ projects/ipu/modules/path/path.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Attaches behaviors for the Path module.
+ */
+
+(function ($) {
+
+    Drupal.behaviors.pathFieldsetSummaries = {
+        attach: function (context) {
+            $('fieldset.path-form', context).drupalSetSummary(function (context) {
+                var path = $('.form-item-path-alias input').val();
+
+                return path ?
+                    Drupal.t('Alias: @alias', {'@alias': path}) :
+                    Drupal.t('No alias');
+            });
+        }
+    };
+
+})(jQuery);
